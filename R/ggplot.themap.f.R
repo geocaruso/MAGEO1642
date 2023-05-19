a55727ef173e22a0537d6be74c4aee648744aabc
geocaruso <geoffrey.caruso@gmail.com>
2023-05-19 12:12:51 +0200

    ggplot with categorical data and examples

--- R/ggplot.themap.f.R
+++ R/ggplot.themap.f.R
@@ -69,7 +69,35 @@ pb2<-ggplot.themap.f(lux102sf_density1821, "DISTRICT", cl.colours = RColorBrewer
 pb2
 
 district.colours=c("Grevenmacher" = "darkolivegreen1", "Luxembourg" = "darkolivegreen3", "Diekirch"="darkolivegreen4", "Wallonie"="orange") 
-pdistr<-ggplot.themap.f(lux102sf_density1821, "DISTRICT", cl.colours = district.colours)
+pdistr<-ggplot.themap.f(lux102sf_density1821, "DISTRICT",
+                        cl.colours = district.colours,
+                        main.title="Luxembourg former districts", leg.title=NULL)
 #As we know Wallonie is not a valid district of Luxembourg and is thus ignored.
 # This is going to be useful for mapping when you don't know before if a category is present or not (e.g. LISA maps)
 pdistr
+
+#Since the retuned outpu is a ggplot object, we can still modify ex-post:
+#For example removing the legend:
+pdistr+ theme(legend.position = "none")
+# This case is useful if one maps many categories,
+#  such as all communes
+pcom<-ggplot.themap.f(lux102sf_density1821,"LAU2",
+                        main.title="Luxembourg 102 communes")
+#The legend would take up the whole map space, so:
+pcom<-pcom+ theme(legend.position = "none")
+
+#Note, theoretically the map could work with only 4 colours.
+#The minimum number of colours is implemented in tmap.
+# tm_shape(lux102sf_density1821) +
+#  tm_polygons(col = "MAP_COLORS", minimize = TRUE)
+
+
+pdf(file="output/Lux_atlas_categorical.pdf")
+print(p)
+print(pb)
+print(plego)
+print(pb2)
+print(pdistr)
+print(pcom)
+dev.off()
+
